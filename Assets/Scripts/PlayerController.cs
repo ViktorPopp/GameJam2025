347071d73ab8c32808142f9aae8aaee4811d9d50
benj381a <118054799+benj381a@users.noreply.github.com>
2025-11-29 13:42:41 +0100

    added falling gravity

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 [RequireComponent (typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    [Header("Playermovement")]
     [SerializeField] private float acceleration = 2000.0f;
     [SerializeField] private float maxSpeed = 10.0f;
     [SerializeField] private float jumpForce = 10.0f;
+
+    [Header("Gravity")]
+    [SerializeField] private float normalGravity = 1f;
+    [SerializeField] private float fallingGravity = 1.5f;
 
     private Rigidbody2D rigidbody;
 
@@ -22,15 +27,19 @@ public class PlayerController : MonoBehaviour
         bool jump = Input.GetKeyDown(KeyCode.Space);
 
         rigidbody.AddForceX(horizontalInput * acceleration * Time.deltaTime, ForceMode2D.Force);
-        if (rigidbody.linearVelocityX > maxSpeed)
-        {
-            rigidbody.linearVelocityX = maxSpeed;
-        }
 
 
         if (jump)
         {
             rigidbody.AddForceY(jumpForce, ForceMode2D.Impulse);
         }
+
+
+
+        if (rigidbody.linearVelocityX > maxSpeed)
+        {
+            rigidbody.linearVelocityX = maxSpeed;
+        }
+        rigidbody.gravityScale = rigidbody.linearVelocityY >= 0 /* positiv */ ? normalGravity : fallingGravity;
     }
 }
