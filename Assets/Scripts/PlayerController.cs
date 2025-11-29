ca7e13b85584a60cf3895178570196a667b52f2b
benj381a <118054799+benj381a@users.noreply.github.com>
2025-11-29 15:27:00 +0100

    added raycast to jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,34 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
+    [Header("Player Movement")]
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 300f;
 
     private Rigidbody2D _rigidbody;
-    [SerializeField] private float _moveHorizontal;
+    private float _moveHorizontal;
+    private bool _jump = false;
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         _moveHorizontal = Input.GetAxisRaw("Horizontal");
+        _jump = Input.GetButtonDown("Jump") && (Physics2D.Raycast(transform.position, Vector2.down, 1, ~(1 << 6)).collider != null);
+        if (_jump)
+        {
+            _rigidbody.AddForceY(jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
         _rigidbody.linearVelocityX = _moveHorizontal * speed;
+        
     }
 }
