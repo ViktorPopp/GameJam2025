b279867506306e329847032b32b33fe82af86de6
Viktor Popp <ViktorPopp.dev@gmail.com>
2025-11-30 07:58:56 +0100

    Added shit

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -1,9 +1,11 @@
+using Unity.Mathematics;
 using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
     public Transform pivot;
     public Transform player;
+    public GameObject bulletPrefab;
     public float radius;
 
     void Start()
@@ -19,5 +21,11 @@ public class GunController : MonoBehaviour
         float angle = Mathf.Atan2(playerVector.y, playerVector.x) * Mathf.Rad2Deg;
 
         pivot.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Quaternion rotation = pivot.rotation * Quaternion.Euler(0, 0, 90);
+            Instantiate(bulletPrefab, transform.position, rotation);
+        }
     }
 }
