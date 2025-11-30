78f2550dbc6c2005404fd93f2abfe7a4b0bdb835
benj381a <118054799+benj381a@users.noreply.github.com>
2025-11-30 01:40:57 +0100

    added gun

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,16 +2,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    [Header("Player Movement")]
+    [SerializeField] private float moveSpeed = 5f;
+
+    [Header("Gun")]
+    [SerializeField] private float distanceToCharacter = 1.5f;
 
     private Rigidbody2D _rigidbody;
     private Vector2 _movement;
     private Animator _animator;
+    private GunController _gunController;
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponentInChildren<Animator>();
+        _gunController = GetComponentInChildren<GunController>();
     }
 
     void Update()
@@ -23,6 +29,15 @@ public class PlayerController : MonoBehaviour
         _rigidbody.linearVelocity = _movement * moveSpeed;
         _animator.SetFloat("VelocityX", _rigidbody.linearVelocityX);
         _animator.SetFloat("VelocityY", _rigidbody.linearVelocityY);
+
+       
+
+        Vector2 _mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float _gunAngle = Vector2.SignedAngle(Vector2.left, (Vector2)transform.position - _mousePos);
+        Vector3 _gunDirection = (transform.forward + new Vector3(Mathf.Cos(_gunAngle * Mathf.Deg2Rad), Mathf.Sin(_gunAngle * Mathf.Deg2Rad), 0)).normalized;
+        _gunController.transform.position = _animator.transform.position + (_gunDirection * distanceToCharacter);
+        _gunController.transform.eulerAngles = Vector3.forward * _gunAngle;
+        _gunController.GetComponent<SpriteRenderer>().flipY = _gunAngle > 90 || _gunAngle < -90;
     }
 
     void FixedUpdate()
